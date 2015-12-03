8e0cd47e3626824cf85f99a494554d0f853f5fe3
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-03 23:23:37 +0900

    create edit view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update]
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to :root
+  end
+
+  private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :position, :profile, :occupation, :avatar)
+  end
+
 end
