5a378cead7764962250de4e109b04e2a6e4fa19c
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-06 01:43:20 +0900

    set routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
 
-  resources :users, only: [:edit, :update]
-  resources :prototypes, only: [:new, :create]
+  resources :users, only: [:edit, :update, :index]
+  resources :prototypes, only: [:new, :create, :show] do
+    collection do
+      get 'popular'
+      get 'newest'
+    end
+  end
 end
