c515bcbabc18712539e946925bb7903a91aaa2fb
Tatsuki-M <tatsukitatsuki0923@gmail.com>
2015-12-11 14:12:41 +0900

    generate comment controller

---
+++ app/controllers/prototypes/comments_controller.rb
@@ -0,0 +1,24 @@
+class Prototypes::CommentsController < ApplicationController
+  before_action :set_prototype, only: :create
+
+  def create
+    @prototype.comments.create(comment_params)
+    redirect_to :back, notice: 'Your comment was successfully create'
+  end
+
+  private
+  def set_prototype
+    @prototype = Prototype.find(prototype_params[:prototype_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(
+      :user_id,
+      :content
+    )
+  end
+
+  def prototype_params
+    params.require(:comment).permit(:prototype_id)
+  end
+end
